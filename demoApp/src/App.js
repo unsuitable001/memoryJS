69423e90a4a5262b9f8a9147de15295afd3b1e6b
James McDonald <15jgm4@queensu.ca>
2019-10-16 22:27:00 -0400

    fixed pr tests from failing

--- demoApp/src/App.js
+++ demoApp/src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
-import Demo from "./components/Demo";
-import "./App.css";
+import React from 'react'
+import Demo from './components/Demo'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="Main Page">
+    <div className='Main Page'>
       <Demo />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- demoApp/src/components/Demo.js
+++ demoApp/src/components/Demo.js
@@ -1,85 +1,85 @@
-import * as memoryJS from "../ReactMemory.js";
-import React from "react";
+// import * as memoryJS from '../ReactMemory.js'
+import React from 'react'
 
 export default class Demo extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      ptrInput: "",
-      ptr: "",
+      ptrInput: '',
+      ptr: '',
       ptrCreated: false,
-      customMemInput: "",
-      customMem: ""
-    };
+      customMemInput: '',
+      customMem: ''
+    }
 
-    this.handlePtrCreate = this.handlePtrCreate.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.deletePtr = this.deletePtr.bind(this);
-    this.changeCustomMem = this.changeCustomMem.bind(this);
+    this.handlePtrCreate = this.handlePtrCreate.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.deletePtr = this.deletePtr.bind(this)
+    this.handleChangeCustomMem = this.handleChangeCustomMem.bind(this)
   }
 
-  handlePtrCreate(event) {
-    console.log("reached");
+  handlePtrCreate (event) {
+    console.log('reached')
     // var pointer = memoryJS.Pointer.publicMemoryObj.newObj(255);
 
-    //Instead set ptr state var to be equal to a pointer to an object of value passed in.
+    // Instead set ptr state var to be equal to a pointer to an object of value passed in.
 
-    var pointer = this.state.ptrInput;
-    this.setState({ ptr: pointer, ptrCreated: true });
-    event.preventDefault();
+    var pointer = this.state.ptrInput
+    this.setState({ ptr: pointer, ptrCreated: true })
+    event.preventDefault()
   }
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+  handleInputChange (event) {
+    const target = event.target
+    const value = target.value
+    const name = target.name
 
     this.setState({
       [name]: value
-    });
+    })
   }
 
-  deletePtr(event) {
-    this.setState({ ptr: "", ptrCreated: true });
-    event.preventDefault();
+  deletePtr (event) {
+    this.setState({ ptr: '', ptrCreated: true })
+    event.preventDefault()
   }
 
-  changeCustomMem(event) {
+  handleChangeCustomMem (event) {
     // create a new pointer at specific mem location
 
-    this.setState({ customMem: this.state.customMemInput });
-    alert("New pointer was created pointing to " + this.state.customMem);
-    event.preventDefault();
+    this.setState({ customMem: this.state.customMemInput })
+    alert('New pointer was created pointing to ' + this.state.customMem)
+    event.preventDefault()
   }
 
-  render() {
+  render () {
     if (!this.state.ptrCreated) {
       return (
         <div>
           <form onSubmit={this.handlePtrCreate}>
             <label>Enter ptr value and click to create: </label>
             <input
-              name="ptrInput"
-              type="number"
+              name='ptrInput'
+              type='number'
               value={this.state.ptrInput}
               onChange={this.handleInputChange}
             />
-            <input type="submit" value="Submit" />
+            <input type='submit' value='Submit' />
           </form>
         </div>
-      );
+      )
     } else {
       return (
         <ul>
           {/* <li><b>Memory Address of created pointer:</b> {this.state.ptr.value()}</li> */}
           <li>
-            <b>Memory Address of created pointer:</b> 0x10000{" "}
+            <b>Memory Address of created pointer:</b> 0x10000{' '}
             <i>(obtained by ptr.value())</i>
           </li>
 
           {/* <li><b>Value stored in pointer is:</b> {this.state.ptr.pointedTo()}</li> */}
           <li>
-            <b>Value stored in pointer is:</b> {this.state.ptr}{" "}
+            <b>Value stored in pointer is:</b> {this.state.ptr}{' '}
             <i>
               Obtained by ptr.pointedTo() or ptr.point or
               memoryJS.publicMemoryObj.valueOf(ptr.value()))
@@ -97,32 +97,32 @@ export default class Demo extends React.Component {
                 </i>
               </label>
               <input
-                name="ptrInput"
-                type="number"
+                name='ptrInput'
+                type='number'
                 value={this.state.ptrInput}
                 onChange={this.handleInputChange}
               />
-              <input type="submit" value="change value" />
+              <input type='submit' value='change value' />
             </form>
           </li>
 
           <li>
             <form>
-              {" "}
+              {' '}
               onSubmit={this.deletePtr}
               {/* update this method to use proper ptr.free */}
               <label>
-                <b>Click to delete pointer</b>{" "}
+                <b>Click to delete pointer</b>{' '}
                 <i>
                   Achieved using ptr.free() or memoryJS.publicMemoryObj.free()
                 </i>
               </label>
-              <input type="submit" value="ptr.free()" />
+              <input type='submit' value='ptr.free()' />
             </form>
           </li>
 
           <li>
-            <form onSubmit={this.changeCustomMem}>
+            <form onSubmit={this.handleChangeCustomMem}>
               {/* Change to method that creates pointer using the proper code */}
               <label>
                 <b>
@@ -131,16 +131,16 @@ export default class Demo extends React.Component {
                 <i>Achieved using var ptr = new Pointer('0x000')</i>
               </label>
               <input
-                name="customMem"
-                type="number"
+                name='customMem'
+                type='number'
                 value={this.state.customMem}
                 onChange={this.handleInputChange}
               />
-              <input type="submit" value="create new pointer" />
+              <input type='submit' value='create new pointer' />
             </form>
           </li>
         </ul>
-      );
+      )
     }
   }
 }
